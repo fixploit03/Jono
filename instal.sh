8d51961516ab619733a0a954455b8f4a5bad40ea
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-11-15 17:17:05 +0700

    Create instal.sh

---
+++ instal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#-------------------------------------------
+# [instal.sh]
+# File ini merupakan bagian dari proyek Jono
+# Dibuat oleh: fixploit03 
+#-------------------------------------------
+# Mau ngapain sih Broh ^_^
+
+chmod +x jono.sh
+cp jono.sh /usr/local/bin/jono
+
+echo "[+] Selesai."
