57d3111b26e851c3cf4b1ef0bdadcd3b8e2670d8
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-11-15 17:06:45 +0700

    Update instal_john.sh

--- instal_john.sh
+++ instal_john.sh
@@ -30,9 +30,10 @@ echo -e ""
 echo -e "${p}╦╔╗╔╔═╗╔╦╗╔═╗╦    ${m}╦╔═╗╦ ╦╔╗╔ ╔╦╗╦ ╦╔═╗ ╦═╗╦╔═╗╔═╗╔═╗╦═╗${r}"
 echo -e "${p}║║║║╚═╗ ║ ╠═╣║    ${m}║║ ║╠═╣║║║  ║ ╠═╣║╣  ╠╦╝║╠═╝╠═╝║╣ ╠╦╝${r}"
 echo -e "${p}╩╝╚╝╚═╝ ╩ ╩ ╩╩═╝ ${m}╚╝╚═╝╩ ╩╝╚╝  ╩ ╩ ╩╚═╝ ╩╚═╩╩  ╩  ╚═╝╩╚═${r}"
+echo ""
 echo -e "${b}[*] ${p}Selamat datang di menu instalasi John The Ripper${r}"
 echo -e "${b}[*] ${p}Pembuat: fixploit03${r}"
-echo -e "${b}[*] ${p}Github: https://github.com/fixploit03/jono/${r}
+echo -e "${b}[*] ${p}Github: https://github.com/fixploit03/jono/${r}"
 echo ""
 
 while true; do
