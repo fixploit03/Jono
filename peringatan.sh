bb9de265c8d3a43e45ce6fca2f4f549223a3737c
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-11-16 00:10:44 +0700

    Create peringatan.sh

---
+++ peringatan.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#-------------------------------------------
+# [peringatan]
+# File ini merupakan bagian dari proyek Jono
+# Dibuat oleh: fixploit03
+#-------------------------------------------
+# Mau ngapain sih Broh ^_^
+
+# Variabel warna
+m="\e[1;31m" # Merah
+h="\e[1;32m" # Hijau
+k="\e[1;33m" # Kuning
+b="\e[1;34m" # Biru
+p="\e[1;37m" # Putih
+r="\e[0m"    # Reset
+
+# Membersihkan layar Terminal
+clear
+
+# Menampilkan kata-kata peringatan
+echo -e "${b}Selamat datang di program Jono${r}"
+echo -e ""
+echo -e "${p}Jono adalah program Bash sederhana yang dirancang untuk melakukan cracking${r}"
+echo -e "${p}terhadap kata sandi file ZIP, RAR, 7Z, PDF, dan file Office (docx, xlsx, pptx)."
+echo -e "${p}Menggunakan John The Ripper.${r}"
+echo -e ""
+echo -e "${k}Peringatan${r}"
+echo -e "${p}----------${r}"
+echo -e ""
+echo -e "${p}Program ini dirancang hanya untuk tujuan pendidikan dan penelitian yang sah.${r}"
+echo -e "${p}Dilarang keras menggunakan program ini untuk kegiatan ilegal, merusak,${r}"
+echo -e "${p}atau tanpa izin pemilik file. Pengguna bertanggung jawab penuh atas segala${r}"
+echo -e "${p}konsekuensi hukum yang mungkin timbul dari penggunaan program ini. Pastikan${r}"
+echo -e "${p}untuk selalu mematuhi peraturan dan etika yang berlaku di wilayah Anda.${r}"
+echo -e ""
+
+sleep 3
