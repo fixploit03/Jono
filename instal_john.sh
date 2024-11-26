f18116406a5489df2f9dde6233fcd27c9fe6929d
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-11-26 21:01:21 +0700

    Create instal_john.sh

---
+++ instal_john.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#-------------------------------------------
+# [instal_jono.sh]
+# File ini merupakan bagian dari proyek Jono
+# Dibuat oleh: fixploit03
+#-------------------------------------------
+# Mau ngapain sih Broh ^_^
+
+# Variabel warna
+m="\e[1;31m" # Merah
+h="\e[1;32m" # Hijau
+k="\e[1;33m" # Kuning
+b="\e[1;34m" # Biru
+p="\e[1;37m" # Putih
+r="\e[0m"    # Reset
+
+# Fungsi untuk mengecek root
+function mengecek_root(){
+	if [[ "$EUID" -ne 0 ]]; then
+        	echo -e "${p}[${m}-${p}] ${p}Program ini harus dijalankan sebagai root.${r}"
+	        exit 1
+	fi
+}
+
+# Fungsi untuk mengecek sistem operasi
+function mengecek_sistem_operasi(){
+	sistem_operasi=$(uname -s)
+	if [[ "${sistem_operasi}" != "Linux" ]]; then
+	        echo -e "${p}[${m}-${p}] Sistem operasi Anda tidak mendukung untuk menjalankan program Jono.${r}"
+        	exit 1
+	fi
+
+	# Mengecek ID Linux
+	file_id_linux="/etc/os-release"
+	if [[ ! -f "${file_id_linux}" ]]; then
+	        echo -e "${p}[${m}-${p}] File '${file_id_linux}' tidak ditemukan. Sistem operasi Anda tidak mendukung untuk menjalankan program Jono.${r}"
+	        exit 1
+	fi
+	. "${file_id_linux}"
+	id_linux=$ID
+	if [[ "${id_linux}" == "kali" ]]; then
+		bash "installer/installer_jono_kali.sh"
+	elif [[ "${id_linux}" == "debian" || "${id_linux}" == "ubuntu" ]]; then
+		bash "installer/installer_jono_deb_ubuntu.sh"
+	else
+	        echo -e "${p}[${m}-${p}] Sistem operasi Anda tidak mendukung untuk menjalankan program Jono.${r}"
+	        exit 1
+	fi
+}
+
+# Fungsi utama
+function utama(){
+	# Memanggil fungsi mengecek_root
+	mengecek_root
+	# Memanggil fungsi mengecek_sistem_operasi
+	mengecek_sistem_operasi
+}
+
+# Memanggil fungsi utama
+utama
