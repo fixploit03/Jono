ce4b63d19919d88fbbc4f85e4a3cde284bc7bfcf
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-11-26 21:36:57 +0700

    Add files via upload

--- installer/installer_jono_deb_ubuntu.sh
+++ installer/installer_jono_deb_ubuntu.sh
@@ -15,7 +15,7 @@ r="\e[0m"    # Reset
 
 # Cek root
 if [[ "$EUID" -ne 0 ]]; then
-        echo -e "${m}[-] ${p}Script ini harus dijalankan sebagai root.${r}"
+        echo -e "${p}[${m}-${p}] Script ini harus dijalankan sebagai root.${r}"
         exit 1
 fi
 
@@ -31,39 +31,39 @@ echo -e "${p}╦╔╗╔╔═╗╔╦╗╔═╗╦    ${m}╦╔═╗╦ �
 echo -e "${p}║║║║╚═╗ ║ ╠═╣║    ${m}║║ ║╠═╣║║║  ║ ╠═╣║╣  ╠╦╝║╠═╝╠═╝║╣ ╠╦╝${r}"
 echo -e "${p}╩╝╚╝╚═╝ ╩ ╩ ╩╩═╝ ${m}╚╝╚═╝╩ ╩╝╚╝  ╩ ╩ ╩╚═╝ ╩╚═╩╩  ╩  ╚═╝╩╚═${r}"
 echo ""
-echo -e "${b}[*] ${p}Selamat datang di menu instalasi John The Ripper${r}"
-echo -e "${b}[*] ${p}Pembuat: fixploit03${r}"
-echo -e "${b}[*] ${p}Github: https://github.com/fixploit03/Jono/${r}"
+echo -e "${p}[${b}*${p}] Selamat datang di menu instalasi John The Ripper${r}"
+echo -e "${p}[${b}*${p}] Pembuat: fixploit03${r}"
+echo -e "${p}[${b}*${p}] Github: https://github.com/fixploit03/Jono/${r}"
 echo ""
 
 # Nanya 
 while true; do
-        read -p $'\e[1;33m[?] \e[1;37mApakah Anda ingin menginstal John The Ripper [Y/n]: \e[1;33m' nanya
+        read -p $'\e[1;37m[\e[1;34m#\e[1;37m] Apakah Anda ingin menginstal John The Ripper [Y/n]: ' nanya
         if [[ -z "${nanya}" ]]; then
-                echo -e "${m}[-] ${p}Masukkan tidak boleh kosong.${r}"
+                echo -e "${p}[${m}-${p}] Masukkan tidak boleh kosong.${r}"
                 continue
         fi
         if [[ "${nanya}" != "y" && "${nanya}" != "Y" && "${nanya}" != "n" && "${nanya}" != "N" ]]; then
-                echo -e "${m}[-] ${p}Masukkan tidak valid. Harap masukkan 'Y/n'.${r}"
+                echo -e "${p}[${m}-${p}] Masukkan tidak valid. Harap masukkan 'Y/n'.${r}"
                 continue
         fi
         break
 done
 
 if [[ "${nanya}" == "n" || "${nanya}" == "N" ]]; then
-        echo -e "${h}[+] ${p}Semoga harimu menyenangkan ^_^${r}"
+        echo -e "${p}[${b}*${p}] Semoga harimu menyenangkan ^_^${r}"
         exit 0
 fi
 
 # Cek koneksi internet
-echo -e "${b}[*] ${p}Mengecek koneksi internet...${r}"
+echo -e "${p}[${b}*${p}] Mengecek koneksi internet...${r}"
 sleep 3
 if ! ping -c 4 8.8.8.8; then
-        echo -e "${m}[-] ${p}Anda tidak memiliki koneksi internet.${r}"
+        echo -e "${p}[${m}-${p}] Anda tidak memiliki koneksi internet.${r}"
         exit 1
 fi
 
-echo -e "${h}[+] ${p}Anda memiliki koneksi internet.${r}"
+echo -e "${p}[${h}+${p}] Anda memiliki koneksi internet.${r}"
 
 # Daftar dependensi yang diperlukan untuk menginstal John the Ripper
 daftar_dependensi=(
@@ -84,78 +84,78 @@ daftar_dependensi=(
 )
 
 # Memperbarui repositori Linux
-echo -e "${b}[*] ${p}Memperbarui repositori Linux...${r}"
+echo -e "${p}[${b}*${p}] Memperbarui repositori Linux...${r}"
 sleep 3
 apt-get update -y
 if [[ $? -ne 0 ]]; then
-        echo -e "${m}[-] ${p}Gagal memperbarui repositori Linux.${r}"
+        echo -e "${p}[${m}-${p}] Gagal memperbarui repositori Linux.${r}"
         exit 1
 fi
 
-echo -e "${h}[+] ${p}Berhasil memperbarui repositori Linux.${r}"
+echo -e "${p}[${h}+${p}] Berhasil memperbarui repositori Linux.${r}"
 
 # Menginstal dependensi yang diperlukan John The Ripper
-echo -e "${b}[*] ${p}Menginstal dependensi yang diperlukan John The Ripper...${r}"
+echo -e "${p}[${b}*${p}] Menginstal dependensi yang diperlukan John The Ripper...${r}"
 sleep 3
 for dependensi in "${daftar_dependensi[@]}"; do
-      echo -e "${b}[*] ${p}Menginstal '${dependensi}'...${r}"
+      echo -e "${p}[${b}*${p}] Menginstal '${dependensi}'...${r}"
       sleep 3
       apt-get install "${dependensi}" -y
       if [[ $? -ne 0 ]]; then
-              echo -e "${m}[-] ${p}Gagal menginstal '${dependensi}'.${r}"
+              echo -e "${p}[${m}-${p}] Gagal menginstal '${dependensi}'.${r}"
               exit 1
       fi
 
-      echo -e "${h}[+] ${p}Berhasil menginstal '${dependensi}'.${r}"
+      echo -e "${p}[${h}+${p}] Berhasil menginstal '${dependensi}'.${r}"
 done
 
 # Mengkloning John The Ripper dari Github
-echo -e "${b}[*] ${p}Mengkloning John The Ripper dari Github...${r}"
+echo -e "${p}[${b}*${p}] Mengkloning John The Ripper dari Github...${r}"
 sleep 3
 cd "${lokasi_instalasi_john}"
 git clone https://github.com/openwall/john
 if [[ $? -ne 0 ]]; then
-        echo -e "${m}[-] ${p}Gagal mengkloning John The Ripper dari Github.${r}"
+        echo -e "${p}[${m}-${p}] Gagal mengkloning John The Ripper dari Github.${r}"
         exit 1
 
 fi
 
-echo -e "${h}[+] ${p}Berhasil mengkloning John The Ripper dari Github.${r}"
+echo -e "${p}[${h}+${p}] Berhasil mengkloning John The Ripper dari Github.${r}"
 
 # Folder saat ini '/usr/share'
 # Masuk ke folder 'src' dari John the ripper
 cd john/src
 
 # Membuat Makefile untuk menginstal John The Ripper...
-echo -e "${b}[*] ${p}Membuat Makefile...${r}"
+echo -e "${p}[${b}*${p}] Membuat Makefile...${r}"
 sleep 3
 ./configure
 if [[ $? -ne 0 ]]; then
-        echo -e "${m}[-] ${p}Gagal membuat Makefile.${r}"
+        echo -e "${p}[${m}-${p}] Gagal membuat Makefile.${r}"
         exit 1
 fi
 
-echo -e "${h}[+] ${p}Berhasil membuat Makefile.${r}"
+echo -e "${p}[${h}+${p}] Berhasil membuat Makefile.${r}"
 
 # Membangun ulang John The Ripper
-echo -e "${b}[*] ${p}Membangun ulang John The Ripper...${r}"
+echo -e "${p}[${b}*${p}] Membangun ulang John The Ripper...${r}"
 sleep 3
 make -s clean && make -sj8
 if [[ $? -ne 0 ]]; then
-        echo -e "${m}[-] ${p}Gagal membangun ulang John The Ripper.${r}"
+        echo -e "${p}[${m}-${p}] Gagal membangun ulang John The Ripper.${r}"
         exit 1
 fi
 
-echo -e "${h}[+] ${p}Berhasil membangun ulang John The Ripper.${r}"
+echo -e "${p}[${h}+${p}] Berhasil membangun ulang John The Ripper.${r}"
 
 # Menginstal John The Ripper
-echo -e "${b}[*] ${p}Menginstal John The Ripper...${r}"
+echo -e "${p}[${b}*${p}] Menginstal John The Ripper...${r}"
 sleep 3
 make install 
 if [[ $? -ne 0 ]]; then
-        echo -e "${m}[-] ${p}Gagal menginstal John The Ripper.${r}"
+        echo -e "${p}[${m}-${p}] Gagal menginstal John The Ripper.${r}"
         exit 1
 fi
 
-echo -e "${h}[+] ${p}Berhasil menginstal John the ripper.${r}"
+echo -e "${p}[${h}+${p}] Berhasil menginstal John the ripper.${r}"
 exit 0
