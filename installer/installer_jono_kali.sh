01fa52c653b127cfed451a4795c433569c73ead9
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-11-30 20:06:41 +0700

    Update installer_jono_kali.sh

--- installer/installer_jono_kali.sh
+++ installer/installer_jono_kali.sh
@@ -64,6 +64,7 @@ echo -e "${p}[${h}+${p}] Anda memiliki koneksi internet.${r}"
 
 # Daftar dependensi yang diperlukan untuk menginstal John the Ripper
 daftar_dependensi=(
+        "systemd"
         "john-data"
         "libcompress-raw-lzma-perl"
 )
