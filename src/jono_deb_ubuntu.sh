3e63c6d5de52e01b1311557ce043f7e930144ae0
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-12-06 16:11:19 +0700

    Update jono_deb_ubuntu.sh

--- src/jono_deb_ubuntu.sh
+++ src/jono_deb_ubuntu.sh
@@ -24,8 +24,16 @@ function mengecek_root(){
 
 # Fungsi untuk mengecek nilai Hash
 function mengecek_nilai_hash(){
-        file_cek_hash="cek_hash.sh"
-	bash "${file_cek_hash}"
+        # File dan Hash
+        deb="src/jono_deb_ubuntu.sh"
+        kali="src/jono_kali.sh"
+        hash_deb="9168fc7f4a3efe9e1ab7b1a3d1f5f4fc"
+        hash_kali="9688bd0f4288bda758c3610f7839590c"
+
+        if [[ $(md5sum "${deb}" | awk '{print $1}') != "${hash_deb}" || $(md5sum "${kali}" | awk '{print $1}') != "${hash_kali}" ]]; then
+          	echo -e "${p}[${m}-${p}] Jangan di recode Broh! kalo mau berkontribusi aja :).${r}"
+        	exit 1
+        fi
 }
 
 # Fungsi untuk membuat folder 'file_hash'
